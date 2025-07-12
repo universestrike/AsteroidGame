1858c30e7c23b93f72ec32cc83ead5a568731471
UniverseStrike <ethan.a.hays@gmail.com>
2025-07-11 17:50:39 -0700

    Add circleshape.py and fix main to run a client and clock

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import pygame
 from constants import *
 
 def main():
+    game = True
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    while game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        time.tick(60)
 
 if __name__ == "__main__":
     main()
